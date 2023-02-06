0475cd3fa2e609757cbab36b89cb719c5c91ac4b
InvictusEd <qqdas522@gmail.com>
2023-02-06 16:01:36 +0800

    购买车票

---
+++ Week3-Cycle/Ticket.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * 循环购买车票
+ */
+public class Ticket {
+    public static void main(String[] args) {
+//        初始化
+        Scanner in = new Scanner(System.in);
+        int balance = 0;
+//        读入投币金额
+        while(true) {
+            System.out.println("请投币：");
+            int amount = in.nextInt();
+            balance = balance + amount;
+            if (balance >= 10) {
+                //        打印车票
+                System.out.println("***************");
+                System.out.println("Java城际铁路专线");
+                System.out.println("   无指定座位票   ");
+                System.out.println("   票价：10元   ");
+                System.out.println("***************");
+                //        计算并打印找零
+                System.out.println("找零：" + (balance - 10));
+                balance = 0;
+            }
+        }
+    }
+}
