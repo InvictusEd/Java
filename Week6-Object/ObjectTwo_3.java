c333e809bac544d64325e88cce75844e56ae6315
InvictusEd <qqdas522@gmail.com>
2023-03-22 20:41:12 +0800

    Math类

---
+++ Week6-Object/ObjectTwo_3.java
@@ -0,0 +1,11 @@
+/**
+ * Math类
+ */
+public class ObjectTwo_3 {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-12));          //绝对值
+        System.out.println(Math.round(10.345));     //四舍五入
+        System.out.println(Math.random()*100);      //0-1的随机数
+        System.out.println(Math.pow(3, 2));         //指数运算
+    }
+}
