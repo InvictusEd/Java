6d3d9351fdfba7e6cf9b71e7553c778eb2e60b46
InvictusEd <qqdas522@gmail.com>
2023-03-22 20:40:12 +0800

    包裹类型 Boolean, Character, Integer, Double

---
+++ Week6-Object/ObjectTwo_1.java
@@ -0,0 +1,11 @@
+/**
+ * 包裹类型 Boolean, Character, Integer, Double
+ */
+public class ObjectTwo_1 {
+    public static void main(String[] args) {
+        int i = 10;         // 基础类型
+        Integer k = 10;     //包裹类型
+        k = i;              //可以赋值
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
