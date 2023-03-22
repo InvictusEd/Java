2fa9d8b3f9d86c587787dd20172587798ee961fb
InvictusEd <qqdas522@gmail.com>
2023-03-22 20:40:50 +0800

    .运算符，擅用包裹类型解决大部分问题

---
+++ Week6-Object/ObjectTwo_2.java
@@ -0,0 +1,10 @@
+/**
+ * .运算符,擅用包裹类型解决大部分问题
+ */
+public class ObjectTwo_2 {
+    public static void main(String[] args) {
+        System.out.println(Character.isDigit('1'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toLowerCase('I'));
+    }
+}
